caab7b1e6c5892623685783706db087b80d2caef
Samira-a <samira@cs.utoronto.ca>
2014-02-06 01:13:39 -0500

    Main
    New Submission

---
+++ config/settings.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+define('__PS_BASE_URI__', '/');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_PS_CACHING_SYSTEM_', 'MCached');
+define('_PS_CACHE_ENABLED_', '0');
+define('_THEME_NAME_', 'theme138');
+define('_DB_NAME_', 'a2585048_shop');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_DB_SERVER_', 'mysql4.000webhost.com');
+define('_DB_USER_', 'a2585048_Doris');
+define('_DB_PREFIX_', 'ps_');
+define('_DB_PASSWD_', 'doris1234');
+define('_DB_TYPE_', 'MySQL');
+define('_COOKIE_KEY_', '4JbW0NZuypnEMffL7waMieDKpwkRPRPaLgwwH82bDW9MF62zJfckvEh8');
+define('_COOKIE_IV_', 'DxhBIMM3');
+define('_RIJNDAEL_KEY_', 'ajaxt7bpN5Dp7KDEvSphQtUA8BGB77Ao');
+define('_RIJNDAEL_IV_', 'muGKbQIcrVBIC6vWvwkZ8g==');
+define('_PS_VERSION_', '1.4.6.2');
+
+?>
